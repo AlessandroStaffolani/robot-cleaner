12d828475c1361e9b6bde019a6ef76f52b1b24b0
Alessandro Staffolani <alessandro.staffolani.93@gmail.com>
2018-07-19 19:50:33 +0200

    Add analysis

---
+++ it.unibo.project/srcMore/it/unibo/ctxConsole/robotroomba.pl
@@ -0,0 +1,13 @@
+%====================================================================================
+% Context ctxConsole  SYSTEM-configuration: file it.unibo.ctxConsole.robotRoomba.pl 
+%====================================================================================
+context(ctxrobot, "localhost",  "TCP", "8032" ).  		 
+context(ctxconsole, "localhost",  "TCP", "8042" ).  		 
+%%% -------------------------------------------
+qactor( robot , ctxrobot, "it.unibo.robot.MsgHandle_Robot"   ). %%store msgs 
+qactor( robot_ctrl , ctxrobot, "it.unibo.robot.Robot"   ). %%control-driven 
+qactor( console , ctxconsole, "it.unibo.console.MsgHandle_Console"   ). %%store msgs 
+qactor( console_ctrl , ctxconsole, "it.unibo.console.Console"   ). %%control-driven 
+%%% -------------------------------------------
+%%% -------------------------------------------
+
