e17bad89f84e437f8c05831da7eeac80440604bf
Alessandro Staffolani <alessandro.staffolani.93@gmail.com>
2018-07-31 15:25:45 +0200

    Update .gitignore

--- it.unibo.project/srcMore/it/unibo/ctxConsoleAnalysis/robotroomba.pl
+++ it.unibo.project/srcMore/it/unibo/ctxConsoleAnalysis/robotroomba.pl
@@ -4,12 +4,10 @@
 context(ctxrobotanalysis, "localhost",  "TCP", "8032" ).  		 
 context(ctxconsoleanalysis, "localhost",  "TCP", "8042" ).  		 
 %%% -------------------------------------------
-qactor( robotexecutoranalysis , ctxrobotanalysis, "it.unibo.robotexecutoranalysis.MsgHandle_Robotexecutoranalysis"   ). %%store msgs 
-qactor( robotexecutoranalysis_ctrl , ctxrobotanalysis, "it.unibo.robotexecutoranalysis.Robotexecutoranalysis"   ). %%control-driven 
-qactor( mindrobotanalysis , ctxrobotanalysis, "it.unibo.mindrobotanalysis.MsgHandle_Mindrobotanalysis"   ). %%store msgs 
-qactor( mindrobotanalysis_ctrl , ctxrobotanalysis, "it.unibo.mindrobotanalysis.Mindrobotanalysis"   ). %%control-driven 
-qactor( testanalysis , ctxconsoleanalysis, "it.unibo.testanalysis.MsgHandle_Testanalysis"   ). %%store msgs 
-qactor( testanalysis_ctrl , ctxconsoleanalysis, "it.unibo.testanalysis.Testanalysis"   ). %%control-driven 
+qactor( robotanalysis , ctxrobotanalysis, "it.unibo.robotanalysis.MsgHandle_Robotanalysis"   ). %%store msgs 
+qactor( robotanalysis_ctrl , ctxrobotanalysis, "it.unibo.robotanalysis.Robotanalysis"   ). %%control-driven 
+qactor( consoleanalysis , ctxconsoleanalysis, "it.unibo.consoleanalysis.MsgHandle_Consoleanalysis"   ). %%store msgs 
+qactor( consoleanalysis_ctrl , ctxconsoleanalysis, "it.unibo.consoleanalysis.Consoleanalysis"   ). %%control-driven 
 %%% -------------------------------------------
 %%% -------------------------------------------
 
